f9875770d076b588210799778c190d71568e5fbf
Hwiyeon Cho <hwyn.cho@gmail.com>
2026-02-27 10:30:42 +0900

    [style] Format treesitter config with stylua
    - stylua 규칙에 맞게 keymap 함수 호출 포맷 정리

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -37,15 +37,24 @@ return {
 
             -- Incremental selection keymaps
             local map = vim.keymap.set
-            map("n", "<C-space>", function()
-                require("nvim-treesitter.incremental_selection").init_selection()
-            end, { desc = "Init treesitter selection" })
-            map("x", "<C-space>", function()
-                require("nvim-treesitter.incremental_selection").node_incremental()
-            end, { desc = "Increment treesitter selection" })
-            map("x", "<bs>", function()
-                require("nvim-treesitter.incremental_selection").node_decremental()
-            end, { desc = "Decrement treesitter selection" })
+            map(
+                "n",
+                "<C-space>",
+                function() require("nvim-treesitter.incremental_selection").init_selection() end,
+                { desc = "Init treesitter selection" }
+            )
+            map(
+                "x",
+                "<C-space>",
+                function() require("nvim-treesitter.incremental_selection").node_incremental() end,
+                { desc = "Increment treesitter selection" }
+            )
+            map(
+                "x",
+                "<bs>",
+                function() require("nvim-treesitter.incremental_selection").node_decremental() end,
+                { desc = "Decrement treesitter selection" }
+            )
         end,
     },
     {
@@ -68,51 +77,105 @@ return {
 
             -- Textobject keymaps
             local map = vim.keymap.set
-            map({ "x", "o" }, "af", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
-            end, { desc = "Select outer function" })
-            map({ "x", "o" }, "if", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
-            end, { desc = "Select inner function" })
-            map({ "x", "o" }, "ac", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
-            end, { desc = "Select outer class" })
-            map({ "x", "o" }, "ic", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
-            end, { desc = "Select inner class" })
-            map({ "x", "o" }, "aa", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
-            end, { desc = "Select outer parameter" })
-            map({ "x", "o" }, "ia", function()
-                require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
-            end, { desc = "Select inner parameter" })
+            map(
+                { "x", "o" },
+                "af",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+                end,
+                { desc = "Select outer function" }
+            )
+            map(
+                { "x", "o" },
+                "if",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+                end,
+                { desc = "Select inner function" }
+            )
+            map(
+                { "x", "o" },
+                "ac",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+                end,
+                { desc = "Select outer class" }
+            )
+            map(
+                { "x", "o" },
+                "ic",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+                end,
+                { desc = "Select inner class" }
+            )
+            map(
+                { "x", "o" },
+                "aa",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+                end,
+                { desc = "Select outer parameter" }
+            )
+            map(
+                { "x", "o" },
+                "ia",
+                function()
+                    require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+                end,
+                { desc = "Select inner parameter" }
+            )
 
             -- Move keymaps
             local move = require "nvim-treesitter-textobjects.move"
-            map({ "n", "x", "o" }, "]m", function()
-                move.goto_next_start("@function.outer", "textobjects")
-            end, { desc = "Next function start" })
-            map({ "n", "x", "o" }, "]M", function()
-                move.goto_next_end("@function.outer", "textobjects")
-            end, { desc = "Next function end" })
-            map({ "n", "x", "o" }, "]]", function()
-                move.goto_next_start("@class.outer", "textobjects")
-            end, { desc = "Next class start" })
-            map({ "n", "x", "o" }, "][", function()
-                move.goto_next_end("@class.outer", "textobjects")
-            end, { desc = "Next class end" })
-            map({ "n", "x", "o" }, "[m", function()
-                move.goto_previous_start("@function.outer", "textobjects")
-            end, { desc = "Previous function start" })
-            map({ "n", "x", "o" }, "[M", function()
-                move.goto_previous_end("@function.outer", "textobjects")
-            end, { desc = "Previous function end" })
-            map({ "n", "x", "o" }, "[[", function()
-                move.goto_previous_start("@class.outer", "textobjects")
-            end, { desc = "Previous class start" })
-            map({ "n", "x", "o" }, "[]", function()
-                move.goto_previous_end("@class.outer", "textobjects")
-            end, { desc = "Previous class end" })
+            map(
+                { "n", "x", "o" },
+                "]m",
+                function() move.goto_next_start("@function.outer", "textobjects") end,
+                { desc = "Next function start" }
+            )
+            map(
+                { "n", "x", "o" },
+                "]M",
+                function() move.goto_next_end("@function.outer", "textobjects") end,
+                { desc = "Next function end" }
+            )
+            map(
+                { "n", "x", "o" },
+                "]]",
+                function() move.goto_next_start("@class.outer", "textobjects") end,
+                { desc = "Next class start" }
+            )
+            map(
+                { "n", "x", "o" },
+                "][",
+                function() move.goto_next_end("@class.outer", "textobjects") end,
+                { desc = "Next class end" }
+            )
+            map(
+                { "n", "x", "o" },
+                "[m",
+                function() move.goto_previous_start("@function.outer", "textobjects") end,
+                { desc = "Previous function start" }
+            )
+            map(
+                { "n", "x", "o" },
+                "[M",
+                function() move.goto_previous_end("@function.outer", "textobjects") end,
+                { desc = "Previous function end" }
+            )
+            map(
+                { "n", "x", "o" },
+                "[[",
+                function() move.goto_previous_start("@class.outer", "textobjects") end,
+                { desc = "Previous class start" }
+            )
+            map(
+                { "n", "x", "o" },
+                "[]",
+                function() move.goto_previous_end("@class.outer", "textobjects") end,
+                { desc = "Previous class end" }
+            )
         end,
     },
 }
